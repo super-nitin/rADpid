2933ca673490c75770eef493aa63183ad6bbe610
Nitin Gupta <git@radpid.com>
2010-11-08 16:40:48 -0800

    Initial commit. git ignore file was updated. standard rails project generated.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RADpid::Application.config.secret_token = '336ab8d632fa39ea9f4d164e5cfd78031d4ca2bf81f16486374dfcf8ba45b26faa84e95fc19c51d71a83fad7dd0f70632680b3b95f7e6f0d4007ee7af4dc8d71'
